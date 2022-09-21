1fbc2faf273852d00592b76eece2554fcd0c3ba3
SonSonder <maceomaddox@gmail.com>
2022-09-20 22:17:10 -0400

    grabbed all of the buttons

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ var userInputEl = document.getElementById("#user-input");
 var searchButtonEl = document.getElementById("#search-button");
 
 var searchedCity1El = document.getElementById("city1-btn");
-var searchedCity1El = document.getElementById("city1-btn");
-var searchedCity1El = document.getElementById("city1-btn");
-var searchedCity1El = document.getElementById("city1-btn");
-var searchedCity1El = document.getElementById("city1-btn");
+var searchedCity2El = document.getElementById("city2-btn");
+var searchedCity3El = document.getElementById("city3-btn");
+var searchedCity4El = document.getElementById("city4-btn");
+var searchedCity5El = document.getElementById("city5-btn");
 
 var searchedCity1Text = document.getElementById("city1-txt");
-var searchedCity1Text = document.getElementById("city1-txt");
-var searchedCity1Text = document.getElementById("city1-txt");
-var searchedCity1Text = document.getElementById("city1-txt");
-var searchedCity1Text = document.getElementById("city1-txt");
+var searchedCity2Text = document.getElementById("city2-txt");
+var searchedCity3Text = document.getElementById("city3-txt");
+var searchedCity4Text = document.getElementById("city4-txt");
+var searchedCity5Text = document.getElementById("city5-txt");
 
 var currentCityEl = document.getElementById("current-city");
 var currentDateEl = document.getElementById("date");
@@ -19,5 +19,47 @@ var currentTempEl = document.getElementById("current-temp");
 var currentWindEl = document.getElementById("current-wind");
 var currentHumidityEl = document.getElementById("current-humidity");
 var currentUvEl = document.getElementById("current-uv");
+
+var day1ImgEl = document.getElementById("day-1-img");
+var day2ImgEl = document.getElementById("day-2-img");
+var day3ImgEl = document.getElementById("day-3-img");
+var day4ImgEl = document.getElementById("day-4-img");
+var day5ImgEl = document.getElementById("day-5-img");
+
+var day1DayEl = document.getElementById("d1-day");
+var day2DayEl = document.getElementById("d2-day");
+var day3DayEl = document.getElementById("d3-day");
+var day4DayEl = document.getElementById("d4-day");
+var day5DayEl = document.getElementById("d5-day");
+
+var day1DateEl = document.getElementById("d1-date");
+var day2DateEl = document.getElementById("d2-date");
+var day3DateEl = document.getElementById("d3-date");
+var day4DateEl = document.getElementById("d4-date");
+var day5DateEl = document.getElementById("d5-date");
+
+var day1TextEl = document.getElementById("d1-text");
+var day2TextEl = document.getElementById("d2-text");
+var day3TextEl = document.getElementById("d3-text");
+var day4TextEl = document.getElementById("d4-text");
+var day5TextEl = document.getElementById("d5-text");
+
+var day1TempEl = document.getElementById("d1-temp");
+var day2TempEl = document.getElementById("d2-temp");
+var day3TempEl = document.getElementById("d3-temp");
+var day4TempEl = document.getElementById("d4-temp");
+var day5TempEl = document.getElementById("d5-temp");
+
+var day1WindEl = document.getElementById("d1-wind");
+var day2WindEl = document.getElementById("d2-wind");
+var day3WindEl = document.getElementById("d3-wind");
+var day4WindEl = document.getElementById("d4-wind");
+var day5WindEl = document.getElementById("d5-wind");
+
+var day1HumidityEl = document.getElementById("d1-humidity");
+var day2HumidityEl = document.getElementById("d2-humidity");
+var day3HumidityEl = document.getElementById("d3-humidity");
+var day4HumidityEl = document.getElementById("d4-humidity");
+var day5HumidityEl = document.getElementById("d5-humidity");
 
 const apiKey = "3c56bb730a2bb2b8fa01960a277c9468";
