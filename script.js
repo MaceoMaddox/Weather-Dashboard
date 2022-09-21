3aca89b3e93a2093a26ca9b79ea24a51fb82bc74
SonSonder <maceomaddox@gmail.com>
2022-09-20 21:32:25 -0400

    setting up these buttons

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 var userInputEl = document.getElementById("#user-input");
 var searchButtonEl = document.getElementById("#search-button");
 
+var searchedCity1El = document.getElementById("city1-btn");
+var searchedCity1El = document.getElementById("city1-btn");
+var searchedCity1El = document.getElementById("city1-btn");
+var searchedCity1El = document.getElementById("city1-btn");
+var searchedCity1El = document.getElementById("city1-btn");
+
+var searchedCity1Text = document.getElementById("city1-txt");
+var searchedCity1Text = document.getElementById("city1-txt");
+var searchedCity1Text = document.getElementById("city1-txt");
+var searchedCity1Text = document.getElementById("city1-txt");
+var searchedCity1Text = document.getElementById("city1-txt");
+
+var currentCityEl = document.getElementById("current-city");
+var currentDateEl = document.getElementById("date");
+var currentTempEl = document.getElementById("current-temp");
+var currentWindEl = document.getElementById("current-wind");
+var currentHumidityEl = document.getElementById("current-humidity");
+var currentUvEl = document.getElementById("current-uv");
+
+const apiKey = "3c56bb730a2bb2b8fa01960a277c9468";
